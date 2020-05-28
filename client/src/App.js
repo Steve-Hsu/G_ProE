7045b73058b50c831939bdfe6ec0df5ee957773f
Steve-Hsu <wuujdsc@gmail.com>
2020-05-28 13:57:47 +0800

    FrontEnd - React basic settings & basic Navbar

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      My App
+    </div>
+  );
+};
+
+export default App;
